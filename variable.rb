76165118b79c55a21d2e394575845145a0e3d4c5
Sean Febo <seanfebo@techie.com>
2018-04-25 22:04:00 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,12 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+class Scraping 
+    
+def self.get_house_bio(url)
+    url = "http://wwo.wikidot.com/subject#toc29"
+    gryf = Gryf.new
+    doc = Nokogiri::HTML(open(url))
+    end
+    binding.pry
+end
